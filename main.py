a50e0d7275fe1cac306bca6288d1838a05ed8a10
Lola Romanenkova <lolita.andreenkova@yandex.ru>
2024-03-02 20:39:01 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,48 @@
+import turtle
+import pandas
+
+screen = turtle.Screen()
+screen.title("U.S. States Game")
+image = "blank_states_img.gif"
+screen.addshape(image)
+turtle.shape(image)
+turtle = turtle.Turtle()
+
+states_data = pandas.read_csv("50_states.csv")
+states = states_data.state.to_list()
+
+
+def get_coordinates():
+    data = (states_data[states_data["state"] == answer_state])
+    return int(data.x), int(data.y)
+
+
+correct_answer = []
+
+while len(correct_answer) < 50:
+    answer_state = screen.textinput(title=f"{ len(correct_answer)}/50 Guess the State",
+                                    prompt="What's another state's name?").title()
+    if answer_state == "Exit":
+        to_learn_states = []
+        for state in states:
+            if state not in correct_answer:
+                to_learn_states.append(state)
+        print(to_learn_states)
+        to_learn_data = pandas.DataFrame(to_learn_states)
+        to_learn_data.to_csv("states_to_learn.csv")
+        break
+
+    if (answer_state in states) and (answer_state not in correct_answer):
+        correct_answer.append(answer_state)
+        turtle.penup()
+        turtle.hideturtle()
+        turtle.goto(get_coordinates())
+        turtle.write(answer_state)
+
+screen.exitonclick()
+#  _____get x and y on a click____
+# def get_mouse_click_coor(x, y):
+#     print(x, y)
+
+# turtle.onscreenclick(get_mouse_click_coor)
+# turtle.mainloop()
